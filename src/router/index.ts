63dd2e05f85547c17f9318fcc883183fc340ec29
Kamal.A_SYDANI <kamalaminu94@gmail.com>
2025-08-06 13:23:54 +0100

    Proj Init

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import AdminView from "@/views/AdminView.vue";
-import LoginView from "@/views/LoginView.vue";
+import AdminView from "../views/AdminView.vue";
+import LoginView from "../views/LoginView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
