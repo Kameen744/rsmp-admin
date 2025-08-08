fc129dfae31b0ab495fbc929990c9a548ff39708
Kamal.A_SYDANI <kamalaminu94@gmail.com>
2025-08-08 14:21:34 +0100

    add hash history

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory,
+} from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AdminView from "../views/AdminView.vue";
 import LoginView from "../views/LoginView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
